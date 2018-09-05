4e58461dd5159c084bc8799086c9bbe68cc880d8
barudisshu <chemnk@163.com>
2018-09-05 17:11:04 +0800

    sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  RouterModule,
+  Routes,
+  Router
+} from '@angular/router';
+import {
+  APP_BASE_HREF,
+  LocationStrategy,
+  HashLocationStrategy
+} from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { ProductImageComponent } from './product-image/product-image.component';
-import { ProductDepartmentComponent } from './product-department/product-department.component';
-import { PriceDisplayComponent } from './price-display/price-display.component';
-import { ProductRowComponent } from './product-row/product-row.component';
-import { ProductsListComponent } from './products-list/products-list.component';
+import { IntroComponent } from './intro/intro.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarItemComponent } from './sidebar/sidebar-item.component';
+
+import { ExampleDef } from './example.model';
+
+/*
+ * Here's the master list of our examples for this chapter.
+ */
+export const examples: ExampleDef[] = [
+   {label: '介绍', name: 'Root', path: '', component: IntroComponent},
+];
+
+const routes: Routes = [
+  { path: '', component: IntroComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductImageComponent,
-    ProductDepartmentComponent,
-    PriceDisplayComponent,
-    ProductRowComponent,
-    ProductsListComponent
+    IntroComponent,
+    SidebarComponent,
+    SidebarItemComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
+
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF,    useValue: '/' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: 'ExampleDefs',    useValue: examples }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
